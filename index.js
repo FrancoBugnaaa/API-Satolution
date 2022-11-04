dc9fdfdab4a2d61a7ac27b6cbeab5905212fd11d
FrancoBugnaaa <101563222+FrancoBugnaaa@users.noreply.github.com>
2022-11-04 15:53:33 -0300

    ...

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const morgan = require('morgan');
 const CookieParser = require('cookie-parser');
 const routes = require('./routes/routes');
-const PORT = 3001;
+
 //Settings
+app.set('port', process.env.Port || 3001);
 app.set('json spaces', 2);
 
 //Midelware
@@ -20,5 +21,5 @@ app.get("/", (req,res)=> {
 })
 //Starting Server
 app.listen(app.get('port'), () => {         
-    console.log(`Server on port ${PORT}`);
+    console.log(`Server on port ${app.get('port')}`);
 }); 
